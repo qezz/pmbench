244b9925ede43e9e98c73b0e7d4d464d849be83b
Sergey Mishin <sergei.a.mishin@gmail.com>
2016-02-07 18:45:36 +0300

    add setting different params

---
+++ test_set_diffparam.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 0 ]]
+then
+    target_folder="$1"
+else
+    target_folder="results"
+fi
+
+for i in `seq 1 60`;
+do
+    time ( sleep 4; crm_resource --resource apache_server --set-parameter test$i --parameter-value $i) 2>>./$target_folder/set-param
+done
+
+# cleanup params
+
+for i in `seq 1 60`
+do
+    crm_resource --resource apache_server --delete-parameter=test$i
+donw
---
+++ test_set_diffparam_cli.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ $# -ne 0 ]]
+then
+    target_folder="$1"
+else
+    target_folder="results"
+fi
+
+for i in `seq 1 60`;
+do
+    time ( sleep 4; crm resource param apache_server set test$i $i) 2>>./$target_folder/set-param-cli
+done
+
+# cleanup params
+
+for i in `seq 1 60`
+do
+    crm resource param apache_server delete test$i
+done
